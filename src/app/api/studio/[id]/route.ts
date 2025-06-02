711d0f63254fd4b763b9211a446096b9a131f895
lazytech614 <derupanjan2021@gmail.com>
2025-06-02 12:57:27 +0530

    added the studio api route

---
+++ src/app/api/studio/[id]/route.ts
@@ -0,0 +1,34 @@
+import { client } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(
+    req: NextRequest,
+    {params: {id}}: {params: {id: string}}
+) {
+    try {
+        const body = await req.json();
+    
+        const studio = await client.user.update({
+            where: {
+                id
+            },
+            data: {
+                studios: {
+                    update: {
+                        screen: body.screen,
+                        mic: body.mic,
+                        preset: body.preset
+                    }
+                }
+            }
+        })
+
+        if(studio)
+            return NextResponse.json({status: 200, message: "Studio updated", data: studio}, {status: 200})
+
+        return NextResponse.json({status: 400, message: "Failed to update the studio settings"}, {status: 400})
+    }catch(err) {
+        console.log("Error in the studio route", err);
+        return NextResponse.json({status: 500, message: "Something went wrong"}, {status: 500})
+    }
+}
