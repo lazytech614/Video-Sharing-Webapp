b790ff679c754be9ce681fd1071725e44ab04fd2
lazytech614 <derupanjan2021@gmail.com>
2025-06-15 20:12:38 +0530

    created the home page

--- src/app/dashboard/[workspaceId]/home/page.tsx
+++ src/app/dashboard/[workspaceId]/home/page.tsx
@@ -1,14 +1,26 @@
 import { getWixContent } from '@/actions/workspace'
+import VideoCard from '@/components/global/videos/video-card'
 import React from 'react'
 
 type Props = {}
 
 const Home = async (props: Props) => {
-    const video = await getWixContent()
-    console.log("Video: ", video);
+    const videos = await getWixContent()
 
   return (
-    <div>Home</div>
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+        {videos.status === 200 ? (
+            videos.data?.map((video: any) => (
+                <VideoCard 
+                    key={video.id} 
+                    {...video} 
+                    workspaceId={video.workSpaceId}
+                />
+            ))
+        ) : (
+            ''
+        )}
+    </div>
   )
 }
 
